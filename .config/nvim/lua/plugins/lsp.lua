c75cdf0a7ab544ddddcbb523ae1e47f39e1580e9
Quan Do <biquando@gmail.com>
2025-06-17 12:22:27 -0700

    Update mason-lspconfig for nvim 11

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -50,23 +50,22 @@ local mason_lspconfig = require('mason-lspconfig')
 
 mason_lspconfig.setup({
   ensure_installed = vim.tbl_keys(servers),
-})
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = servers[server_name],
+        filetypes = (servers[server_name] or {}).filetypes,
+      })
+    end,
 
-mason_lspconfig.setup_handlers({
-  function(server_name)
-    require('lspconfig')[server_name].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    })
-  end,
-
-  ['clangd'] = function()
-    require('lspconfig').clangd.setup({
-      cmd = { 'clangd', '--clang-tidy' },
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-  end
+    ['clangd'] = function()
+      require('lspconfig').clangd.setup({
+        cmd = { 'clangd', '--clang-tidy' },
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+    end
+  },
 })
